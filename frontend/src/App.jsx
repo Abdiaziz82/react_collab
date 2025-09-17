649f0b5e2f28bd3218f30ffc6de1b2ad68fd0bcf
AMIRKIAYI <amirjuma036@gmail.com>
2025-09-17 16:47:28 +0300

    footer updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Footer from './Components/Footer'
 function App() {
   return (
     <>
       <h1 className='text-3xl font-bold underline text-red-500 text-center'>HELLO COLLABORATERS</h1>
-     
+     <Footer/>  
     </>
   )
 }
