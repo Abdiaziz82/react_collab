6fa4283657d527eb3973344d862b2bf20332ae37
Abdiaziz77 <abdiaziznoor20@gmail.com>
2025-09-17 17:25:49 +0300

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
-import Footer from './Components/Footer'
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import AboutPage from "./Components/About";
+import LoginPage from "./Components/Login";
+import Service from "./Components/Service";
+import Products from "./Components/product";
+
 function App() {
   return (
-    <>
-     
-     <Footer/>  
-</>
-
-import AboutPage from './Components/About'
-
-     
     
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar */}
+        <Navbar />
 
-import Navbar from './Components/Navbar.jsx'
-function App() {
-  return (
-    <>
-    
-    <Navbar />
-      <AboutPage />
+        {/* Page Content */}
+        <main className="flex-grow">
+          <Routes>
+           
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/services" element={<Service />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<LoginPage />} />
+            
 
+          </Routes>
+        </main>
 
-    </>
-  )
+        {/* Footer */}
+        <Footer />
+      </div>
+   
+  );
 }
 
-export default App
+export default App;
